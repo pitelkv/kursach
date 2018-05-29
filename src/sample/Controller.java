4bb520f91581b28d2af34a67ad4823ddf470ddda
pitelkv <pitelkv@gmail.com>
2018-05-30 00:05:16 +0500

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,27 @@
+package sample;
+
+        import javafx.fxml.FXML;
+        import java.awt.event.ActionEvent;
+        import java.net.URL;
+        import java.util.ResourceBundle;
+
+public class Controller {
+
+
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void calculate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+}
